fba5675cf403146d1ddee02d2b7338e16172e801
Brian Blakely <anewpage.media@gmail.com>
2018-03-23 11:37:57 -0400

    Display celsius option.

--- src/index.js
+++ src/index.js
@@ -5,23 +5,46 @@
 import 'file-loader?name=[name].[ext]!./index.html';
 import './index.scss';
 
-import {  } from './js/visual/visual.js';
+import './js/visual/visual.js';
+import * as Dom from './js/dom.js';
 import render from './js/render.js';
+
+let tempUnit = `f`;
+let weatherData = undefined;
+
+Dom.unit.addEventListener(`click`, ()=> {
+  const {classList} = Dom.unit;
+
+  classList.toggle(`active`);
+
+  tempUnit =
+    classList.contains(`active`)
+      ? `c`
+      : `f`;
+
+  render(tempUnit, weatherData);
+});
 
 render();
 
 // Get the user's location and display appropriate weather info.
 navigator.geolocation.getCurrentPosition((position) => {
-  // fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${
-  //   position.coords.latitude
-  // }&lon=${
-  //   position.coords.longitude
-  // }&APPID=8d0f401501a283d54a9f3c1dfc698466`)
-  //   .then(function(response) {
-  //     return response.json();
-  //   })
-  //   .then(function(api) {
-  //   });
-}, (err)=> console.error(`err`, err));
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${
+    position.coords.latitude
+  }&lon=${
+    position.coords.longitude
+  }&APPID=8d0f401501a283d54a9f3c1dfc698466`)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(apiData) {
+      weatherData = apiData;
+      render(tempUnit, apiData);
+    });
+}, (err)=> {
+  console.info(`err`, err);
+
+  render(tempUnit);
+});
 
 
--- src/js/dom.js
+++ src/js/dom.js
@@ -1,6 +1,7 @@
 // Cache static DOM elements.
 
 export const
+      unit = document.getElementById(`unit`),
       current = document.getElementById(`current`),
       week = document.getElementById(`week`),
       visual = document.getElementById(`visual`);
